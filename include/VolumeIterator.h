49846fa5d9af88d90ac215ef4cda30c0b6883e6c
Matt Williams <matt@milliams.com>
2007-09-06 13:55:21 +0000

    Linux fix: Case-sensitive filenames

--- include/VolumeIterator.h
+++ include/VolumeIterator.h
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "OgrePrerequisites.h"
 #include "OgreSharedPtr.h"
 
-#include "Typedef.h"
+#include "TypeDef.h"
 
 namespace Ogre
 {
