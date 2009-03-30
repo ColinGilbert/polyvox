a719aec2e7de93a7793eb6bcb6f70401e6852229
David Williams <david@david-williams.info>
2009-03-30 22:02:26 +0000

    Small linux fix

--- examples/OpenGL/Shapes.cpp
+++ examples/OpenGL/Shapes.cpp
@@ -2,7 +2,7 @@
 
 using namespace PolyVox;
 
-void createSphereInVolume(Volume<PolyVox::uint8_t>& volData, float fRadius, uint8_t uValue)
+void createSphereInVolume(Volume<PolyVox::uint8_t>& volData, float fRadius, PolyVox::uint8_t uValue)
 {
 	//This vector hold the position of the center of the volume
 	Vector3DFloat v3dVolCenter(volData.getSideLength() / 2, volData.getSideLength() / 2, volData.getSideLength() / 2);
@@ -30,7 +30,7 @@ void createSphereInVolume(Volume<PolyVox::uint8_t>& volData, float fRadius, uint
 	}
 }
 
-void createCubeInVolume(Volume<PolyVox::uint8_t>& volData, Vector3DUint16 lowerCorner, Vector3DUint16 upperCorner, uint8_t uValue)
+void createCubeInVolume(Volume<PolyVox::uint8_t>& volData, Vector3DUint16 lowerCorner, Vector3DUint16 upperCorner, PolyVox::uint8_t uValue)
 {
 	//This three-level for loop iterates over every voxel between the specified corners
 	for (int z = lowerCorner.getZ(); z <= upperCorner.getZ(); z++)
