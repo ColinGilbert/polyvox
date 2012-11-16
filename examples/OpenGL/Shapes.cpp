ef34c5d88951a20c9e29f1cc5633dbaf0138a382
Daviw Williams <david@volumesoffun.com>
2012-11-16 10:50:46 +0100

    Fixed conversion warning.

--- examples/OpenGL/Shapes.cpp
+++ examples/OpenGL/Shapes.cpp
@@ -57,8 +57,8 @@ void createSphereInVolume(LargeVolume<MaterialDensityPair44>& volData, float fRa
 
 void createCubeInVolume(LargeVolume<MaterialDensityPair44>& volData, Vector3DInt32 lowerCorner, Vector3DInt32 upperCorner, uint8_t uValue)
 {
-	int maxDen = MaterialDensityPair44::getMaxDensity();
-	int minDen = MaterialDensityPair44::getMinDensity();
+	uint8_t maxDen = MaterialDensityPair44::getMaxDensity();
+	uint8_t minDen = MaterialDensityPair44::getMinDensity();
 	//This three-level for loop iterates over every voxel between the specified corners
 	for (int z = lowerCorner.getZ(); z <= upperCorner.getZ(); z++)
 	{
