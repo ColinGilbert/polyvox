03de39b8dd26f6142bc288292cd39dd3de1cf6bf
Matt Williams <matt@milliams.com>
2012-12-26 15:37:16 +0000

    Add some documentation to the CompilerCapabilities.h files

--- library/PolyVoxCore/include/PolyVoxCore/Impl/CompilerCapabilities.h
+++ library/PolyVoxCore/include/PolyVoxCore/Impl/CompilerCapabilities.h
@@ -1,3 +1,11 @@
+/*
+ * This file provides the default compiler capabilities for for Visual Studio.
+ * On other compilers CMake will detect which features are available and create
+ * a file like this.
+ * 
+ * To Enable these features in Visual Studio, define the variables in this file.
+*/
+
 #ifndef __PolyVox_CompilerCapabilities_H__
 #define __PolyVox_CompilerCapabilities_H__
 
