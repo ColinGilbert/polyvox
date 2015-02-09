c62bde0066e33d593a087dacd2fac08214fa7c4a
David Williams <david@volumesoffun.com>
2015-02-09 22:11:06 +0100

    Work on getting the OpenGL/Qt5 support working with new header-only library.

--- examples/Basic/OpenGLWidget.h
+++ examples/Basic/OpenGLWidget.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*******************************************************************************
 Copyright (c) 2005-2009 David Williams
 
@@ -69,3 +70,74 @@ private:
 };
 
 #endif //__BasicExample_OpenGLWidget_H__
+=======
+/*******************************************************************************
+Copyright (c) 2005-2009 David Williams
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+claim that you wrote the original software. If you use this software
+in a product, an acknowledgment in the product documentation would be
+appreciated but is not required.
+
+2. Altered source versions must be plainly marked as such, and must not be
+misrepresented as being the original software.
+
+3. This notice may not be removed or altered from any source
+distribution. 	
+*******************************************************************************/
+
+#ifndef __BasicExample_OpenGLWidget_H__
+#define __BasicExample_OpenGLWidget_H__
+
+#include "PolyVoxCore/SurfaceMesh.h"
+
+#include "glew/glew.h"
+
+#include <QGLWidget>
+#include <QOpenGLShaderProgram>
+
+class OpenGLWidget : public QGLWidget
+{
+public:
+	//Constructor
+	OpenGLWidget(QWidget *parent);
+
+	//Mouse handling
+	void mouseMoveEvent(QMouseEvent* event);
+	void mousePressEvent(QMouseEvent* event);
+
+	//Convert a SurfaceMesh to OpenGL index/vertex buffers
+	void setSurfaceMeshToRender(const PolyVox::SurfaceMesh<PolyVox::PositionMaterial>& surfaceMesh);
+
+protected:
+	//Qt OpenGL functions
+	void initializeGL();
+	void resizeGL(int w, int h);
+	void paintGL();
+
+private:
+	//Index/vertex buffer data
+	GLuint noOfIndices;
+	GLuint indexBuffer;
+	GLuint vertexBuffer;
+	GLuint vertexArrayObject;
+	
+	QOpenGLShaderProgram shader;
+
+	//Mouse data
+	QPoint m_LastFrameMousePos;
+	QPoint m_CurrentMousePos;
+	int m_xRotation;
+	int m_yRotation;
+};
+
+#endif //__BasicExample_OpenGLWidget_H__
+>>>>>>> parent of 2da902d... This change reverts PolyVox back to using Qt 4.8. This is expected to be a temporary change, required because moving to Qt 5 caused various complications on Windows which we are not yet ready to address.
