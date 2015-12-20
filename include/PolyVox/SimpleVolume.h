1e9fdf074ab870845e98b03dcc3eae8737b7513d
David Williams <david@volumesoffun.com>
2015-12-20 21:22:29 +0000

    Removed the old LargeVolume and SimpleVolume classes, which are now replaced by PagedVolume.

--- include/PolyVox/SimpleVolume.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PolyVox_SimpleVolume_H__
-#define __PolyVox_SimpleVolume_H__
-
-#pragma message("WARNING - The SimpleVolume class has been replaced by PagedVolume. Please use that instead.")
-
-#include "PagedVolume.h"
-
-#endif //__PolyVox_SimpleVolume_H__
